38c88d00ea3c3395af342d02c959434e8f13c621
harmeetkaur <harmeet.kaur@nagarro.com>
2021-06-22 12:39:39 +0530

    done

---
+++ src/main/java/com/nagarro/employeemanagementapi/EmployeeManagementApiApplication.java
@@ -0,0 +1,14 @@
+package com.nagarro.employeemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+	}
+
+
+}
